0191ffcd8576b8d122932e043cef754d2582e411
jyoti jha <sjyotijha34@gmai.com>
2025-05-30 19:08:40 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:  '/netflix_react/',
+  
   plugins: [react(),tailwindcss()],
+  base: '/netflix_react/'
 })
